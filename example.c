c163e8cd2fe6ba5f916522fc6ea720439dea1d2b
= <=>
2017-02-16 05:31:59 +0000

    Updated to reflect testing procedure.

--- example.c
+++ example.c
@@ -11,6 +11,9 @@
 / be mentioned shortly.
 *******************************************************************************/
 int main() {
+
+	int x = 0;
+
 	printf("Initalizing GPIO...\n\n");
 	initGPIO();  // Setup GPIO and motorControl. Must be called before use!	
 	while(!motorReady()){}; // Wait for the thread to be ready.
@@ -18,7 +21,7 @@ int main() {
 	// * * * * * * * * * * * * Timing test * * * * * * * * * * * * * * * * * * *
 	
 	// Forward
-	printf("Calling move forward at speed 5...\n");
+	printf("Calling move forward.\n\n");
 	clock_t strt = clock(), diff;	// Current CPU time
 	moveForward(5);					// Tell CanSat to move forward at spd 5
 	diff = (clock() - strt);		// Time since function call
@@ -28,9 +31,21 @@ int main() {
 	msec2 = diff/1000.0;			// Convert to millisecons.
 	printf("Function call Time:  %f microseconds.\n",msec);
 	printf("Motor Command Time:  %f milliseconds.\n",msec2);
-	
+
+	while(x < 11) {
+		printf("\nMoving forward at speed %d\n",x);
+		moveForward(x);
+		delay(1000);
+		x++;
+	}
+
+	halt();
+
+	printf("Press Enter to begin the Move Backwards Test...");
+	getchar();
+
 	// Backward
-	printf("\nCalling move backward at speed 4...\n");
+	printf("\nCalling move backward.\n\n");
 	strt = clock();				// Current CPU time
 	moveBackward(10);			// Tell CanSat to move backward
 	diff = (clock() - strt);	// Time that occured during call
@@ -41,60 +56,77 @@ int main() {
 	printf("Function call Time:  %f microseconds.\n",msec);
 	printf("Motor Command Time:  %f milliseconds.\n",msec2);	
 	
-	// Halt
-	printf("\nCalling halt...\n");
-	strt = clock();				// Current CPU time.
-	halt();						// Tell CanSat to stop.
-	diff = (clock() - strt);	// Time that occured during call
-	msec = diff;				// Microseconds
-	while(!motorReady()){}		// Wait for thread to finish
-	diff = (clock() - strt);	// Time that occured since call
-	msec2 = diff/1000.0;		// Convert to milliseconds.
-	printf("Function call Time:  %f microseconds.\n",msec);
-	printf("Motor Command Time:  %f milliseconds.\n",msec2);
-	
-	// Turn Right
-	printf("\nCalling turn right hard at angle 90...\n");
-	strt = clock();				// Current CPU time.
-	turnRightHard(90);			// Tell CanSat to turn right hard.
-	diff = (clock() - strt);	// Time that occured during call
-	msec = diff;				// Microseconds
-	while(!motorReady()){}		// Wait for thread to finish
-	diff = (clock() - strt);	// Time that occured since call
-	msec2 = diff/1000.0;		// Convert to milliseconds.
-	printf("Function call Time:  %f microseconds.\n",msec);
-	printf("Action completed (dependent on angle, speed):  %f milliseconds.\n",msec2);
-	
-	// Turn Right Soft 90 degrees.
-	printf("\nCalling turn right Soft at angle 90...\n");
-	strt = clock();				// Current CPU time.
-	turnRightSoft(90);			// Tell CanSat to turn right.
-	diff = (clock() - strt);	// Time that occured during call
-	msec = diff;				// Microseconds
-	while(!motorReady()){}		// Wait for thread to finish
-	diff = (clock() - strt);	// Time that occured since call
-	msec2 = diff/1000.0;		// Convert to milliseconds.
-	printf("Function call Time:  %f microseconds.\n",msec);
-	printf("Action completed (dependent on angle, speed):  %f milliseconds.\n",msec2);
-	printf("Press Enter to run motor forward at all speeds.");
-	getchar();
-	int x = 0;
-	while(x < 11) {
-		printf("\nMoving forward at speed %d\n",x);
-		moveForward(x);
-		delay(3000);
-		x++;
-	}
-	halt();
-	printf("Press Enter to run motor backward at all speeds.");
-	getchar();
 	x = 0;
 	while(x < 11) {
 		printf("\nMoving backward at speed %d\n",x);
 		moveBackward(x);
-		delay(3000);
+		delay(1000);
 		x++;
 	}
+
+	halt();
+
+	printf("Press Enter to turn right hard 20 degrees\n");
+	getchar();
+	turnRightHard(20);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn right hard 45 degrees\n");
+	getchar();
+	turnRightHard(45);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn right hard 90 degrees\n");
+	getchar();
+	turnRightHard(90);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn right soft 20 degrees\n");
+	getchar();
+	turnRightSoft(20);
+	while (!motorReady()){}
+
+	printf("Press Enter to turn right soft 45 degrees\n");
+	getchar();
+	turnRightSoft(45);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn right soft 90 degrees\n");
+	getchar();
+	turnRightSoft(90);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn left hard 20 degrees\n");
+	getchar();
+	turnLeftHard(20);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn left hard 45 degrees\n");
+	getchar();
+	turnLeftHard(45);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn left hard 90 degrees\n");
+	getchar();
+	turnLeftHard(90);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn left soft 20 degrees\n");
+	getchar();
+	turnLeftSoft(20);
+	while (!motorReady()){}
+
+	printf("Press Enter to turn left soft 45 degrees\n");
+	getchar();
+	turnLeftSoft(45);
+	while(!motorReady()){}
+
+	printf("Press Enter to turn left soft 90 degrees\n");
+	getchar();
+	turnLeftSoft(90);
+	while(!motorReady()){}
+	
 	printf("Testing complete!\n");
 	return 0;
+
 }//main
